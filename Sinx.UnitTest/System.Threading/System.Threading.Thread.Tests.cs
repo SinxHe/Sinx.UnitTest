4a694f301f14f7f07e7877c7769ca09578454560
SinxHe <SinxHe@qq.com>
2017-03-15 15:06:02 +0800

    for thread communication - AutoResetEvent,ManualResetEvent

--- Sinx.UnitTest/System.Threading/System.Threading.Thread.Tests.cs
+++ Sinx.UnitTest/System.Threading/System.Threading.Thread.Tests.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Security.Principal;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
-using ThreadState = System.Threading.ThreadState;
 
 namespace Sinx.UnitTest.System.Threading
 {
@@ -30,7 +24,7 @@ namespace Sinx.UnitTest.System.Threading
 			ParameterizedThreadStart func = obj => ((string[])obj)[0] = "Func<object>"; // Func<object>
 			string[] param = { nameof(ParameterizedThreadStart) };
 
-			Thread thread0 = new Thread(func);
+			//Thread thread0 = new Thread(func);
 			// 传入 null
 			//thread0.Start(); NullReferenceException
 
@@ -48,7 +42,7 @@ namespace Sinx.UnitTest.System.Threading
 			Assert.True(threadId > 0);
 
 			// 是否为 后台线程
-			bool isBackground = Thread.CurrentThread.IsBackground;
+			//bool isBackground = Thread.CurrentThread.IsBackground;
 			// Assert.False(isBackground);		// 这个在调试和运行的时候是后台线程, 在使用"实时调试"的时候是前台线程
 			// 前台线程: 前台线程执行完程序才能结束
 			// 后台线程: 程序可以直接结束不管后台线程的执行状态
@@ -57,8 +51,8 @@ namespace Sinx.UnitTest.System.Threading
 			bool isAlive = Thread.CurrentThread.IsAlive;            // 是否 "活着"
 			Assert.True(isAlive);
 
-			ThreadState state = Thread.CurrentThread.ThreadState;   // 运行状态
-																	// Assert.Equal(state, ThreadState.Running);	// 实时调试 - 前台线程 -> ThreadState.Running	|	调试/运行 - 后台线程 -> ThreadState.Background
+			//ThreadState state = Thread.CurrentThread.ThreadState;   
+			// Assert.Equal(state, ThreadState.Running);	// 实时调试 - 前台线程 -> ThreadState.Running	|	调试/运行 - 后台线程 -> ThreadState.Background
 
 			ThreadPriority priority = Thread.CurrentThread.Priority;// 优先级
 			Assert.Equal(priority, ThreadPriority.Normal);          // Highest > AboveNormal > Normal > BelowNormal > Lowest
@@ -67,8 +61,9 @@ namespace Sinx.UnitTest.System.Threading
 			Assert.False(isThreadProolThread);
 
 			// 当前线程的各种上下文信息
-			ExecutionContext executionContext = Thread.CurrentThread.ExecutionContext;  // todo 
-																						// 当前线程使用的语言文化
+			// todo 
+			ExecutionContext executionContext = Thread.CurrentThread.ExecutionContext;
+			// 当前线程使用的语言文化
 			CultureInfo culture = Thread.CurrentThread.CurrentCulture;
 			Assert.Equal(culture.ToString(), "zh-CN");
 		}
@@ -115,10 +110,11 @@ namespace Sinx.UnitTest.System.Threading
 			context.OperationStarted();                     // 空方法, 在派生类中重写以响应操作 开始 执行的回调
 			context.OperationCompleted();                   // 空方法, 在派生类中重写以响应操作 完成 执行的回调
 			var b = context.IsWaitNotificationRequired();   // 确定是否等待通知
-															// SendOrPostCallback - Action<object> - 表示在消息即将被调度到同步上下文时要调用的方法。
-															//context.Post((SendOrPostCallback)null, (object)null);	// 在派生类中重写, 将异步消息分派到同步上下文
-															//context.Send((SendOrPostCallback)null, (object)null);
-															//context.Wait((IntPtr[])null, waitAll: true, millisecondsTimeout: 1000);	// 等待数组中的任意元素或所有元素接收信号
+
+			// SendOrPostCallback - Action<object> - 表示在消息即将被调度到同步上下文时要调用的方法。
+			//context.Post((SendOrPostCallback)null, (object)null);	// 在派生类中重写, 将异步消息分派到同步上下文
+			//context.Send((SendOrPostCallback)null, (object)null);
+			//context.Wait((IntPtr[])null, waitAll: true, millisecondsTimeout: 1000);	// 等待数组中的任意元素或所有元素接收信号
 		}
 
 		[Theory]
@@ -177,7 +173,7 @@ namespace Sinx.UnitTest.System.Threading
 					Thread.Sleep(500);
 					ar[0] = 1;
 				}
-				finally		// 其实在 lock 中帮忙做了这个
+				finally     // 其实在 lock 中帮忙做了这个
 				{
 					Monitor.Exit(exeAr);
 				}
@@ -197,7 +193,7 @@ namespace Sinx.UnitTest.System.Threading
 			thd1.Start();
 			Thread.Sleep(100);
 			thd2.Start();
-			Thread.Sleep(100);	// 恢复线程2的时候要保证当前线程不会锁住exeAr
+			Thread.Sleep(100);  // 恢复线程2的时候要保证当前线程不会锁住exeAr
 			Monitor.Enter(exeAr);
 			try
 			{
@@ -207,6 +203,59 @@ namespace Sinx.UnitTest.System.Threading
 			{
 				Monitor.Exit(exeAr);
 			}
+		}
+
+		[Fact]
+		public void ThreadCommunication_AutoResetEvent()
+		{
+			// AutoResetEvent 从 signaled -> unsignaled 会自动执行(一旦它激活了一个线程)
+			// ManualResetEvent 可以激活任意多个线程直到手动调用 Reset() 方法
+			var are = new AutoResetEvent(false);    // 线程间通信
+			var ar = new[] { 0 };
+			Thread t1 = new Thread(() =>
+			{
+				ar[0] = 1;
+				are.WaitOne();  // 等待AutoResetEvent信号, 如果信号已经发了, 这里就直接执行不用等待
+				ar[0] = 2;
+				are.WaitOne();
+				ar[0] = 3;
+			});
+			t1.Start();
+			Thread.Sleep(1000);
+			Assert.Equal(1, ar[0]);
+			are.Set();  // 发射信号
+			Thread.Sleep(500);
+			Assert.Equal(2, ar[0]);
+			Thread.Sleep(500);
+			Assert.NotEqual(3, ar[0]);
+			are.Set();
+			Thread.Sleep(500);
+			Assert.Equal(3, ar[0]);
+		}
+
+		[Fact]
+		public void ThreadCommunication_ManualResetEvent()
+		{
+			// AutoResetEvent 从 signaled -> unsignaled 会自动执行(一旦它激活了一个线程)
+			// ManualResetEvent 可以激活任意多个线程直到手动调用 Reset() 方法
+			var mre = new ManualResetEvent(false);    // 线程间通信
+			var ar = new[] { 0 };
+			Thread t1 = new Thread(() =>
+			{
+				ar[0] = 1;
+				mre.WaitOne();  // 等待AutoResetEvent信号, 如果信号已经发了, 这里就直接执行不用等待
+				ar[0] = 2;
+				mre.WaitOne();
+				ar[0] = 3;
+			});
+			t1.Start();
+			Thread.Sleep(1000);
+			Assert.Equal(1, ar[0]);
+			mre.Set();  // 发射信号
+			Thread.Sleep(500);
+			Assert.Equal(3, ar[0]);
+			Thread.Sleep(500);
+			Assert.Equal(3, ar[0]);
 		}
 	}
 }
