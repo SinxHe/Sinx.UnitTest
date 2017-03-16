2dcb9017701d504f61615a0c93d9a32d9ec80e98
SinxHe <SinxHe@qq.com>
2017-03-16 13:53:16 +0800

    thread pool test

---
+++ Sinx.UnitTest/System.Threading/System.Threading.ThreadPool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sinx.UnitTest.System.Threading
+{
+    public class ThreadPoolTest
+    {
+		[Fact]
+	    public void ThreadPool_Enqueue()
+		{
+			var ar = new[] {0};
+			// 创建工作项
+			WaitCallback workItem = obj => ar[0] = 1;
+			// 加入线程池队列 queue v. 排列
+			ThreadPool.QueueUserWorkItem(workItem);
+			Thread.Sleep(500);
+			Assert.Equal(ar[0], 1);
+		}
+    }
+}
