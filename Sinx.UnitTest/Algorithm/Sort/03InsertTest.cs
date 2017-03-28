179a01ce47b36d7a4bdbed55adfdd91c703caeb7
SinxHe <SinxHe@qq.com>
2017-03-28 13:02:33 +0800

    insert sort test

---
+++ Sinx.UnitTest/Algorithm/Sort/03InsertTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sinx.UnitTest.Algorithm.Sort
+{
+	// ReSharper disable once InconsistentNaming
+	public class _03InsertTest
+	{
+		[Fact]
+		public void Insert()
+		{
+			var ar = Vars.Ar.ToList();
+			var arSorted = Vars.ArSorted;
+			for (int i = 0; i < ar.Count; i++)
+			{
+				for (int j = 0; j <= i; j++)    // = for 0 situation
+				{
+					if (ar[i] <= ar[j])
+					{
+						var t = ar[i];
+						ar.RemoveAt(i);
+						ar.Insert(j, t);
+					}
+				}
+			}
+			Assert.Equal(ar, arSorted);
+		}
+	}
+}
