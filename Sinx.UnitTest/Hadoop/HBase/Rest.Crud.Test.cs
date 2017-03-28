d23cfa2cc4421c64bb929200a1341e204becce69
SinxHe <SinxHe@qq.com>
2017-03-28 10:23:20 +0800

    hbase insert table row test

--- Sinx.UnitTest/Hadoop/HBase/Rest.Crud.Test.cs
+++ Sinx.UnitTest/Hadoop/HBase/Rest.Crud.Test.cs
@@ -14,6 +14,8 @@ namespace Sinx.UnitTest.HBase
 	public class Crud
 	{
 		private readonly HttpClient _client;
+		private readonly Func<string> _getNowDateTime = () => DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+		private readonly Func<string, string> _toBase64 = str => Convert.ToBase64String(Encoding.UTF8.GetBytes(str));
 		public Crud()
 		{
 			_client = new HttpClient
@@ -34,8 +36,8 @@ namespace Sinx.UnitTest.HBase
 		[Fact]
 		public void Rest_Post_CreateTable()
 		{
-			var tableName = "TableFromSinxUnitTest" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
-			var columnFamily = "cf" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+			var tableName = "TableFromSinxUnitTest" + _getNowDateTime();
+			var columnFamily = "cf" + _getNowDateTime();
 			var request = new HttpRequestMessage(HttpMethod.Post, $"/{tableName}/schema");
 			request.Headers.Add("Accept", "text/xml");
 			request.Content = new StringContent(
@@ -47,8 +49,22 @@ namespace Sinx.UnitTest.HBase
 			Assert.Equal(response.StatusCode, HttpStatusCode.Created);
 		}
 
+		[Theory]
+		[InlineData("t1", "r1", "f1:c1")]
+		public void Rest_Put_InsertTableRow(string tableName, string row, string column)
+		{
+			var value = _getNowDateTime(); //Convert.ToBase64String(Encoding.UTF8.GetBytes(_getNowDateTime()));
+			var request = new HttpRequestMessage(HttpMethod.Put, $"/{tableName}/{row}");
+			request.Headers.Add("Accept", "application/json");
+			request.Content = new StringContent($"{{\"Row\":[{{\"key\":\"{_toBase64(row)}\", \"Cell\": [{{\"column\":\"{_toBase64(column)}\", \"$\":\"{_toBase64(value)}\"}}]}}]}}");
+			request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			var task = _client.SendAsync(request);
+			var response = task.Result;
+			Assert.Equal(response.StatusCode, HttpStatusCode.OK);
+		}
+
 		[Fact]
-		public void Rest_Post_UpdateTableSchema()
+		public void Rest_Put_UpdateTableSchema()
 		{
 		}
 
