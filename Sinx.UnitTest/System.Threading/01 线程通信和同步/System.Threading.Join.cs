36c732fd766560e2959c998ad162014f53a0a777
SinxHe <SinxHe@qq.com>
2017-04-10 13:18:44 +0800

    阻塞调用线程等待被调用线程执行完成

---
+++ Sinx.UnitTest/System.Threading/01 线程通信和同步/System.Threading.Join.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sinx.UnitTest.System.Threading._01_线程通信和同步
+{
+	/// <summary>
+	/// 让一个线程等待另一个线程执行完成
+	/// </summary>
+	public class JoinTest
+	{
+		[Fact]
+		public void Join_WaitAnotherThreadComplete()
+		{
+			var num = 0;
+			var thread = new Thread(() =>
+			{
+				Thread.Sleep(TimeSpan.FromSeconds(2));
+				num = 1;
+			});
+			thread.Start();
+			thread.Join();	// 阻止调用线程直到线程终止
+			Assert.Equal(num, 1);
+		}
+	}
+}
