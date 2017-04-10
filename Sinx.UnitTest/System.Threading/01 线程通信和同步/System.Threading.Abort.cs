45fb789b01b0e328d3e84b639b8ce083d51f3986
SinxHe <SinxHe@qq.com>
2017-04-10 13:32:04 +0800

    终止线程Abort

---
+++ Sinx.UnitTest/System.Threading/01 线程通信和同步/System.Threading.Abort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sinx.UnitTest.System.Threading._01_线程通信和同步
+{
+	public class ThreadAbortTest
+	{
+		[Fact]
+		public void Thread_Abort_AbortAThread()
+		{
+			int n = 0;
+			var thread = new Thread(() => n++);
+			thread.Start();
+			Thread.Sleep(TimeSpan.FromSeconds(1));
+			thread.Abort();	// 引发 ThreadAbortException 异常
+			// 调用线程调用 Abort 方法, 给被调用线程注入了 ThreadAbortException 异常
+			// 这非常危险, 因为异常可以在任何时间发生, 这可能会彻底摧毁引用程序
+			// 异常也不一定总能被终止, 目标线程可以通过处理该异常并调用Thread.ResetAbort方法来拒绝被终止
+			// 使用CancellationToken来取消线程执行比较好
+		}
+	}
+}
