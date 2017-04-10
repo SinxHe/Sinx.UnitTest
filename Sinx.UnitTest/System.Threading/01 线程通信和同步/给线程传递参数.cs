f162759ff9cbc62c55da0d220a189f0da57885eb
SinxHe <SinxHe@qq.com>
2017-04-10 17:26:26 +0800

    给线程传递参数的三种方式

---
+++ Sinx.UnitTest/System.Threading/01 线程通信和同步/给线程传递参数.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sinx.UnitTest.System.Threading._01_线程通信和同步
+{
+	public class 给线程传递参数
+	{
+		[Fact]
+		public void Thread_DeliverParams_Lambda()
+		{
+			int n = 1;
+			var thread = new Thread(() => n++);
+			// Lambda表达式定义了一个不属于任何类的方法
+			// 这里Lambda表达式在使用任何局部变量的时候, 编译器会帮我们把局部变量封装到一个匿名类的属性上
+			// 而这个局部变量是不存在的, 这里的局部变量和Lambda中使用的变量都是匿名类的属性
+			thread.Start();
+			thread.Join();
+			Assert.Equal(n, 2);
+		}
+
+		[Fact]
+		public void Thread_DeliverParams_ClassDeliver()
+		{
+			var deliverClass = new DeliverClass { N = 1};
+			var thread = new Thread(deliverClass.PlusPlus);
+			// 这里的实现方式跟 int n = 1; var thread = new Thread(() => n++); 一模一样, 只不过这里的匿名类是自己创建的非匿名类而已
+			// 注意: 如果这里的调用改成了var thread = new Thread(() => deliverClass.PlusPlus()); 那么还是会再创建一个匿名类, 将匿名类的Lambda实现方法赋值给Thread(Action)
+			thread.Start();
+			thread.Join();
+			Assert.Equal(deliverClass.N, 2);
+		}
+
+		[Fact]
+		public void Thread_DeliverParams_StartMethod()
+		{
+			var thread = new Thread(o => Assert.Equal(o, 1));
+			thread.Start(1);
+			// 取消装箱的结果是无法改变的
+			// 使用Start传值的方式对于结果的传出很麻烦
+		}
+
+		private class DeliverClass
+		{
+			public int N { get; set; }
+
+			public void PlusPlus()
+			{
+				N++;
+			}
+		}
+	}
+}
