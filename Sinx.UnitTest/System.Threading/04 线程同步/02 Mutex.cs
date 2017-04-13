e5480b60f0d9a9e9443668d524a90050a024e9c1
SinxHe <SinxHe@qq.com>
2017-04-13 17:40:10 +0800

    线程同步-Mutex-避免条件竞争

---
+++ Sinx.UnitTest/System.Threading/04 线程同步/02 Mutex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable InconsistentNaming
+
+namespace Sinx.UnitTest.System.Threading._04_线程同步
+{
+	public class _02_Mutex
+	{
+		/// <summary>
+		/// Mutex是一种原始的同步方式, 其只对一个线程授予访问共享资源的独占访问
+		/// </summary>
+		[Fact]
+		public void Synchronization_Mutex_AvoidRaceCondition()
+		{
+			const string mutexName = nameof(mutexName);
+			using (var mutex = new Mutex(false, mutexName))
+			{
+				if (!mutex.WaitOne(TimeSpan.FromSeconds(2), false))
+				{
+					Console.WriteLine("第二个实例正在运行...");
+				}
+				else
+				{
+					Console.WriteLine();
+				}
+			}
+		}
+	}
+}
