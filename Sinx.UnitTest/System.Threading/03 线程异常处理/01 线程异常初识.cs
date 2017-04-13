06bc25c98594714bf76afcb8d4c559304103cee5
SinxHe <SinxHe@qq.com>
2017-04-13 16:16:24 +0800

    线程同步 - Interlocked 原子操作

---
+++ Sinx.UnitTest/System.Threading/03 线程异常处理/01 线程异常初识.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable InconsistentNaming
+
+namespace Sinx.UnitTest.System.Threading._03_线程异常处理
+{
+	public class _01_线程异常初识
+	{
+		/// <summary>
+		/// 多线程在回调抛出的异常并不能被调用方法捕获, 事实上, 调用方法可能已经执行完毕
+		/// 如果调用线程在等待回调执行完毕, 那么在回调方法抛出异常之后, 目标线程变成了Stopped状态,
+		/// 调用线程正常执行, 好像没有发生异常一样
+		/// </summary>
+		[Fact]
+		public void Thread_Exception_ThrowAExceptionInCallBack()
+		{
+			var t = new Thread(ThrowAndCatch);
+			t.Start();
+			t.Join();
+
+			var isInCatchBlock = false;
+			try
+			{
+				t = new Thread(Throw);
+				t.Start();
+				t.Join();
+			}
+			catch (Exception)
+			{
+				isInCatchBlock = true;
+			}
+			Assert.False(isInCatchBlock);
+
+			void Throw()
+			{
+				Thread.Sleep(TimeSpan.FromSeconds(2));
+				throw new Exception("Boom!");
+			}
+
+			void ThrowAndCatch()
+			{
+				var isInCatch = false;
+				try
+				{
+					Throw();
+				}
+				catch (Exception)
+				{
+					// 一般来说不要在线程中抛出异常而是在线程中使用try..catch块
+					// 当然, 在异步中对此的处理是将异常信息包装在了状态机中进行返回
+					isInCatch = true;
+				}
+				finally
+				{
+					Assert.True(isInCatch);
+				}
+			}
+		}
+	}
+}
---
+++ Sinx.UnitTest/System.Threading/04 线程同步/01 Interlocked-执行基本原子操作避免条件竞争.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable InconsistentNaming
+
+namespace Sinx.UnitTest.System.Threading._04_线程同步
+{
+	public class _01_执行基本原子操作避免条件竞争
+	{
+		[Fact]
+		public void AtomicOperate_AvoidRaceCondition()
+		{
+			// -----------------------------------
+			// 条件竞争
+			var count = 0;
+			void Operate()
+			{
+				for (var i = 0; i < 100000; i++)
+				{
+					count++;
+					count--;
+				}
+			}
+			var thread1 = new Thread(Operate);
+			var thread2 = new Thread(Operate);
+			var thread3 = new Thread(Operate);
+			var threads = new List<Thread> { thread1, thread2, thread3};
+			threads.ForEach(t => t.Start());
+			threads.ForEach(t => t.Join());
+			Assert.NotEqual(count, 0);
+			// -----------------------------------
+			// 原子操作 - 线程同步 - 避免条件竞争
+			var number = 0;
+
+			void AtomicOperate()
+			{
+				for (var i = 0; i < 100000; i++)
+				{
+					Interlocked.Increment(ref number);	// 递增原子操作
+					Interlocked.Decrement(ref number);	// 递减原子操作  
+				}
+			}
+
+			var t1 = new Thread(AtomicOperate);
+			var t2 = new Thread(AtomicOperate);
+			var t3 = new Thread(AtomicOperate);
+			var ts = new List<Thread> { t1, t2, t3};
+			ts.ForEach(t => t.Start());
+			ts.ForEach(t => t.Join());
+			Assert.Equal(number, 0);
+		}
+	}
+}
