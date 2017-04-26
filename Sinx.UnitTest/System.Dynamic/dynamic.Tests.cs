e718ee718804d2848e16c8d7efce0fd5f2326254
HeabKing <heabking@gmail.com>
2017-04-26 18:44:13 +0800

    dynamic

--- Sinx.UnitTest/System.Dynamic/dynamic.Tests.cs
+++ Sinx.UnitTest/System.Dynamic/dynamic.Tests.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.CSharp.RuntimeBinder;
 using Xunit;
 
@@ -13,6 +12,30 @@ namespace Sinx.UnitTest.System.Dynamic
 	// [被测试的方法(s)]_[假设条件]_[期望的表现]
 	public class DynamicTests
 	{
+		/// <summary>
+		/// 对于局部变量dynamic
+		/// </summary>
+		/// <remarks>
+		/// 只生成object类型IL, 但是不会使用DynamicAttribute进行标记
+		/// </remarks>
+		[Fact]
+		public void Dynamic_LocalVar_WithoutDynamicAttribute()
+		{
+			// IL => object d = 123;
+			// 局部dynamic变量payload(特殊的IL)没有DynamicAttribute, 因为它限制在方法内部使用
+			// DynamicAttribute 指明一个Object是动态分发类型
+			dynamic d = 123;
+		}
+
+		private dynamic _d;
+		[Theory]
+		[InlineData(456)]
+		public void Dynamic_Property_UseDynamicAttribute(dynamic d)
+		{
+			// _d 和 d 由于都添加了DynamicAttribute标识
+			_d = d;
+		}
+
 		/// <summary>
 		/// dynamic In: 普通类型 Result: 使用反射
 		/// </summary>
@@ -24,32 +47,149 @@ namespace Sinx.UnitTest.System.Dynamic
 		{
 			object set = new[] { 1, 2, 3 };
 			dynamic d = set;
-			Assert.Equal(d.Length, 3);
+			// 可以看到IL中: ldstr        "Length", 说明使用反射在调用
+			var length = d.Length;
+			Assert.Equal(length, 3);
+		}
+
+		[Fact]
+		public void Dynamic_WrapObjectWithGetMetaDataType_UseGetMetaObjectMethod()
+		{
+			dynamic d = new ExpandoObject();    // 实现了IDynamicMetaObjectProvider, 所以应该调用GetMetaObject方法而不是使用反射
+			d.Name = "sinx";
+			var name1 = d.Name; // object
+			string name2 = d.Name;  // string
+									// TODO: 这里好像使用的也是反射啊
+		}
+
+		private static readonly int StaticMember = 1;
+		/// <summary>
+		/// dynamic 访问静态成员 - 异常
+		/// 其实正常代码中也只能使用 SomeType.StaticMember
+		/// </summary>
+		[Fact]
+		public void Dynamic_Static_GetStaticMember_Fail()
+		{
+			dynamic d = this;
+			//Assert.Equal(this.StaticMember, 1);
+			Assert.Equal(StaticMember, 1);  // 不能通过实例调用, 但是在方法中直接访问可以
+			Assert.Throws(typeof(RuntimeBinderException), () => d.StaticMember);
 		}
 
 		/// <summary>
-		/// dynamic In: Delegate Condition: Type Cast Result: Successful  
+		/// dynamic访问静态成员
 		/// </summary>
 		/// <remarks>
-		/// 普通类型(固定类型)不可以给属性赋委托, 动态分发类型可以給属性赋委托
+		/// dynamic 限制只能访问对象的实例成员, 所以这里添加了拓展
 		/// </remarks>
 		[Fact]
-		public void Dynamic_AssignedDelegate_NeedTypeInference()
+		public void Dynamic_InvokeStaticMember()
 		{
-			object o = (Func<int, int>)(a => 2 * a);
-			//dynamic d = a => 2;
-			dynamic d = (Func<int, int>)(a => 2 * a);
-			Assert.Equal(d(0), 0);
-			Assert.Equal(d(1), 2);
-			Assert.Throws<RuntimeBinderException>(() =>
+			dynamic stringType = new StaticMemberDynamicWrapper(typeof(string));
+			var r = stringType.Concat("A", "B");	// 动态调用String的静态Concat方法
+			Assert.Equal("AB", r);
+
+			dynamic d = new StaticMemberDynamicWrapper(this.GetType());
+			Assert.Equal(d.StaticMember, 1);
+		}
+
+		private sealed class StaticMemberDynamicWrapper : DynamicObject
+		{
+			private readonly TypeInfo _typeInfo;
+
+			public StaticMemberDynamicWrapper(Type type)
 			{
-				d.del = (Func<int, int>) (a => 2 * a);
-			});
-			dynamic d2 = new ExpandoObject();
-			d2.Name = "测试各种属性添加";
-			d2.del = (Func<int, int>)(a => 2 * a);
-			Assert.Equal(d2.Name, "测试各种属性添加");
-			Assert.Equal(d2.del(2), 4);
+				_typeInfo = type.GetTypeInfo();
+			}
+
+			public override IEnumerable<string> GetDynamicMemberNames()
+			{
+				return _typeInfo.DeclaredMembers.Select(m => m.Name);
+			}
+
+			public override bool TryGetMember(GetMemberBinder binder, out object result)
+			{
+				result = null;
+				var field = FindField(binder.Name);
+				if (field != null)
+				{
+					result = field.GetValue(null);
+					return true;
+				}
+
+				var prop = FindProperty(binder.Name, true);
+				if (prop != null)
+				{
+					result = prop.GetValue(null, null);
+					return true;
+				}
+				return false;
+			}
+
+			public override bool TrySetMember(SetMemberBinder binder, object value)
+			{
+				var field = FindField(binder.Name);
+				if (field != null)
+				{
+					field.SetValue(null, value);
+					return true;
+				}
+				var prop = FindProperty(binder.Name, false);
+				if (prop != null)
+				{
+					prop.SetValue(null, value, null);
+					return true;
+				}
+				return false;
+			}
+
+			public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+			{
+				MethodInfo method = FindMethod(binder.Name, args.Select(c => c.GetType()).ToArray());
+				if (method == null)
+				{
+					result = null;
+					return false;
+				}
+				result = method.Invoke(null, args);
+				return true;
+			}
+
+			private MethodInfo FindMethod(string name, Type[] paramTypes)
+			{
+				return _typeInfo.DeclaredMethods.FirstOrDefault(mi => mi.IsPublic  
+					&& mi.IsStatic 
+					&& mi.Name == name 
+					&& ParametersMatch(mi.GetParameters(), paramTypes));
+			}
+
+			private bool ParametersMatch(ParameterInfo[] parameters, Type[] paramTypes)
+			{
+				if (parameters.Length != paramTypes.Length)
+				{
+					return false;
+				}
+				return !parameters.Where((t, i) => t.ParameterType != paramTypes[i]).Any();
+			}
+
+			private PropertyInfo FindProperty(string name, bool get)
+			{
+				if (get)
+				{
+					return _typeInfo.DeclaredProperties.FirstOrDefault(
+						pi => pi.Name == name && pi.GetMethod != null &&
+						pi.GetMethod.IsPublic && pi.GetMethod.IsStatic);
+				}
+				return _typeInfo.DeclaredProperties.FirstOrDefault(
+					pi => pi.Name == name && pi.SetMethod != null && 
+					pi.SetMethod.IsPublic && pi.SetMethod.IsPublic);
+			}
+
+			private FieldInfo FindField(string name)
+			{
+				return _typeInfo.DeclaredFields.FirstOrDefault(fi => fi.IsPublic && fi.IsStatic
+					&& fi.Name == name);
+			}
 		}
 	}
 }
