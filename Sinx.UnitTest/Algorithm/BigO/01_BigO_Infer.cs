9abd26a33776b190373d6e12a170d7d672a8b0e2
SinxHe <sinxhe@qq.com>
2017-04-27 23:08:36 +0800

    BigO Infer

---
+++ Sinx.UnitTest/Algorithm/BigO/01_BigO_Infer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable InconsistentNaming
+
+namespace Sinx.UnitTest.Algorithm.BigO
+{
+	internal class _01_BigO_Compute
+	{
+		/// <summary>
+		/// 推断大O时间复杂度
+		/// </summary>
+		[Fact]
+		public void BigO_Infer()
+		{
+			int n = 10;
+			// 常数阶 - O(1)
+			var r = n * n;	// 计算次数与n无关
+			// 线性阶 - O(n)
+			for (int i = 0; i < n; i++)
+			{
+				// 每次前进常数步
+			}
+			// 对数阶 - O(lgn)
+			for (int i = 0; i < n; i *= 2)
+			{
+				// 每次前进步数线性增长
+			}
+			// 平方阶 - O(n方)
+			for (int i = 0; i < n; i++)
+			{
+				// 每次前进常数步
+				for (int j = 0; j < n; j++)
+				{
+					// 每次前进常数步
+				}
+			}
+		}
+	}
+}
