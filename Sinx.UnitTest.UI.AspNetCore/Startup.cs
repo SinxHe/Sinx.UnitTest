97069e2f5b595253ad8745cd7dee6023c60522ab
SinxHe <sinxhe@qq.com>
2017-05-22 22:48:25 +0800

    call mysql procedure use dapper

--- Sinx.UnitTest.UI.AspNetCore/Startup.cs
+++ Sinx.UnitTest.UI.AspNetCore/Startup.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using Microsoft.Extensions.DependencyInjection;
---
+++ Sinx.UnitTest/Db/MySql/ProcedureTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace Sinx.UnitTest.Db.MySql
+{
+	public class ProcedureTests
+	{
+		private const string ConnString = "Server=neter.me;Port=3305;Database=TestDB;Uid=SinxHe;Pwd=SinxHe*#7370#;";
+		private readonly IDbConnection _db = new MySqlConnection(ConnString);
+		[Fact]
+		public async Task Procedure_Create()
+		{
+			var d = new DynamicParameters();
+			d.Add("@avgage", dbType: DbType.Int32, direction: ParameterDirection.Output);
+			_db.Query("GET_AvgAge", d, commandType: CommandType.StoredProcedure);
+			var age = d.Get<int>("@avgage");
+		}
+	}
+}
