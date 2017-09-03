71946ffe47a5ecc70662d663bcd9ef04e87f342f
heshixiong <heshixiong@gridsum.com>
2017-09-03 22:58:24 +0800

    logging test

---
+++ Sinx.UnitTest/Microsoft.Extensions/Logging/LoggingTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinx.UnitTest.Microsoft.Extensions.Logging
+{
+	public class LoggingTest
+	{
+		public void UseInConsoleTest()
+		{
+			var serviceCollection = new ServiceCollection();
+		}
+	}
+}
