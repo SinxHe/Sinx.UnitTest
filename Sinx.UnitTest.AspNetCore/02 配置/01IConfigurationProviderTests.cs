8f5a9cefc0559e28c4ca48f2fdd569e50c2d2e1f
heshixiong <heshixiong@gridsum.com>
2017-09-27 14:04:17 +0800

    CounfigurationProvider & Configuration Source Tests

---
+++ Sinx.UnitTest.AspNetCore/02 配置/01IConfigurationProviderTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration.CommandLine;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.FileProviders;
+using Xunit;
+using Microsoft.Extensions.Configuration.Memory;
+
+namespace Sinx.UnitTest.AspNetCore._02_配置
+{
+	public class _01IConfigurationProviderTests
+	{
+		[Fact]
+		public void IConfigurationProvider_JsonConfigurationProvider()
+		{
+			var fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+			var source = new JsonConfigurationSource
+			{
+				FileProvider = fileProvider,
+				// 这里应该是相对fileProvider的路径而不是绝对路径, 如果是绝对路径, 即时文件存在且是fileProvider根目录下的也是NotFoundException
+				Path = "appsettings.json",
+				Optional = false,
+				ReloadOnChange = false
+			};
+			var provider = new JsonConfigurationProvider(source);
+			provider.TryGet("Name", out var name0);
+			Assert.Null(name0);
+			provider.Load();
+			provider.TryGet("Name", out var name1);
+			Assert.Equal(name1, "SinxHe");
+		}
+
+		[Fact]
+		public void IConfigurationProvider_MemoryConfigurationProvider()
+		{
+			var source = new MemoryConfigurationSource
+			{
+				InitialData = new Dictionary<string, string>
+				{
+					["Name"] = "SinxHe",
+					["ConnectionStrings:Ali"] = "{AliConnectionString}"
+				}
+			};
+			var provider = new MemoryConfigurationProvider(source);
+			provider.TryGet("Name", out var name0);
+			// !!!
+			Assert.Equal(name0, "SinxHe");
+			provider.Load();
+			provider.TryGet("Name", out var name1);
+			Assert.Equal(name1, "SinxHe");
+		}
+
+		[Fact]
+		public void IConfigurationProvider_EnvironmentVariablesConfigurationProvider()
+		{
+			Environment.SetEnvironmentVariable("Name", "SinxHe");
+			var source = new EnvironmentVariablesConfigurationSource
+			{
+				Prefix = null
+			};
+			var provider = new EnvironmentVariablesConfigurationProvider();
+			provider.TryGet("Name", out var name);
+			Assert.Null(name);
+			provider.Load();
+			provider.TryGet("Name", out name);
+			Assert.Equal(name, "SinxHe");
+		}
+
+		[Fact]
+		public void IConfigurationProvider_CommandLine()
+		{
+			var args = new[] { "--name", "SinxHe" };
+			var map = new Dictionary<string, string>
+			{
+				["--name"] = "Name"
+			};
+			var source = new CommandLineConfigurationSource
+			{
+				Args = args,
+				SwitchMappings = map
+			};
+			var provider = new CommandLineConfigurationProvider(args, map);
+			provider.TryGet("Name", out var name);
+			Assert.Null(name);
+			provider.Load();
+			provider.TryGet("Name", out name);
+			Assert.Equal(name, "SinxHe");
+		}
+	}
+}
---
+++ Sinx.UnitTest.AspNetCore/02 配置/02Configuration_Source_Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Sinx.UnitTest.AspNetCore._02_配置
+{
+    public class _02Configuration_Source_Tests
+    {
+	    private readonly ConfigurationBuilder _configurationBuilder;
+		public _02Configuration_Source_Tests()
+		{
+			_configurationBuilder = new ConfigurationBuilder();
+		}
+
+		[Fact]
+	    public void Configuration_Source_JsonFile_Test()
+	    {
+		    var conf = _configurationBuilder
+			    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			    .Build();
+		    string name = conf["Name"];
+		    Assert.Equal(name, "SinxHe");
+		}
+
+		[Fact]
+	    public void Configuration_Source_Memory_Test()
+	    {
+		    var kvs = new Dictionary<string, string>
+		    {
+			    ["Name"] = "SinxHe"
+		    };
+		    var conf = _configurationBuilder
+			    .AddInMemoryCollection(kvs)
+				.Build();
+		    string name = conf["Name"];
+		    Assert.Equal(name, "SinxHe");
+	    }
+
+		[Fact]
+	    public void Configuration_Source_EnvironmentVariable()
+		{
+			Environment.SetEnvironmentVariable("Name", "SinxHe");
+			Environment.SetEnvironmentVariable("ConnectionStrings:Ali", "AliConnectionString");
+			var conf = _configurationBuilder
+				.AddEnvironmentVariables()
+				.Build();
+			var name = conf["Name"];
+			Assert.Equal(name, "SinxHe");
+			var connStr = conf.GetSection("ConnectionStrings").GetSection("Ali").Value;
+			Assert.Equal(connStr, "AliConnectionString");
+		}
+
+		[Fact]
+	    public void Configuration_Source_CommondLine()
+		{
+			var args = new[] { "-n", "SinxHe"};
+			var map = new Dictionary<string, string>
+			{
+				{ "-n", "Name"}
+			};
+			var conf = _configurationBuilder
+				.AddCommandLine(args, map)
+				.Build();
+			var name = conf["Name"];
+			Assert.Equal(name, "SinxHe");
+		}
+    }
+}
