35c2e0b473db060a4b7460c23eeb0d2c63ae98c1
heshixiong <heshixiong@gridsum.com>
2017-09-27 14:35:39 +0800

    ConfigrationRoot JsonFile Reload Test

--- Sinx.UnitTest.AspNetCore/02 配置/03IConfigurationRootTests.cs
+++ Sinx.UnitTest.AspNetCore/02 配置/03IConfigurationRootTests.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.PlatformAbstractions;
 using Xunit;
 
 namespace Sinx.UnitTest.AspNetCore._02_配置
@@ -68,6 +72,29 @@ namespace Sinx.UnitTest.AspNetCore._02_配置
 			Assert.NotNull(connStr);
 			var brother = conf.GetSection("Family").GetChildren().First().GetSection("Name").Value;
 			Assert.Equal(brother, "Brother");
+		}
+
+		[Fact]
+	    public void IConfigurationRoot_Reload_JsonFile_DirectlyGet()
+		{
+			var path = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "appsettings.json");
+			var conf = new ConfigurationBuilder()
+				.AddJsonFile(path, false, true)
+				.Build();
+			var name = conf["Name"];
+			Assert.Equal(name, "SinxHe");
+
+			var content = File.ReadAllText(path);
+			File.WriteAllText(path, content.Replace("SinxHe", "DaXiong"));
+
+			name = conf["Name"];
+			Assert.NotEqual(name, "DaXiong");
+			conf.Reload();
+			// 使用另一个线程进行重载, 所以这个时候要等一下, 等他重载完成
+			// 如果没有重载完成就进行获取, 会获取到null
+			Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+			name = conf["Name"];
+			Assert.Equal(name, "DaXiong");
 		}
     }
 }
