42c8c4ecc2a70f549ceb5f109bcdd870e37e6f04
heshixiong <heshixiong@gridsum.com>
2017-10-20 16:35:48 +0800

    razor测试, 路径这里有点问题还没解决

---
+++ Sinx.UnitTest.AspNetCore/03 Razor/RazorRenderFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+
+namespace Sinx.UnitTest.AspNetCore._03_Razor
+{
+    public class RazorRenderFactory
+    {
+		public static IRender Create()
+		{
+			var sc = new ServiceCollection();
+			sc.AddSingleton(PlatformServices.Default.Application);
+			var appDir = Directory.GetCurrentDirectory();
+			var hostEnvironment = new HostingEnvironment
+			{
+				 WebRootFileProvider = new PhysicalFileProvider(appDir)
+			};
+			sc.AddSingleton<IHostingEnvironment>(hostEnvironment);
+			sc.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+			sc.AddSingleton<DiagnosticSource>(new DiagnosticListener("Microsoft.AspNetCore"));
+			sc.AddLogging();
+			sc.AddMvc();
+			sc.AddSingleton<IRender, RazorRender>();
+			var sp = sc.BuildServiceProvider();
+			return sp.GetService<IRender>();
+		}
+    }
+}
---
+++ Sinx.UnitTest.AspNetCore/03 Razor/StartupTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sinx.UnitTest.AspNetCore._03_Razor
+{
+	public class StartupTest
+	{
+		[Fact]
+		public async Task Razor_RenderAsync()
+		{
+			var render = RazorRenderFactory.Create();
+			var content = await render.RenderAsync(Path.Combine(Directory.GetCurrentDirectory(), "03 Razor", "Template.cshtml"), Enumerable.Range(0, 9));
+			File.WriteAllText("View.html", content);
+		}
+
+	}
+}
