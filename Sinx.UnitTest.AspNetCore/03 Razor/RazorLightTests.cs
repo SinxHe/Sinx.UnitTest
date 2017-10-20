db936580eea11ba77a7983c9b8ac6412b1bd0c30
heshixiong <heshixiong@gridsum.com>
2017-10-20 17:15:12 +0800

    RazorLight控制台测试没问题, 单元测试有问题

---
+++ Sinx.UnitTest.AspNetCore/03 Razor/RazorLightTests.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Linq;
+using RazorLight;
+using Xunit;
+
+namespace Sinx.UnitTest.AspNetCore._03_Razor
+{
+    public class RazorLightTests
+    {
+		[Fact]
+		public void RazorLight_Render()
+		{
+			var engine = new EngineFactory().ForFileSystem(Path.Combine(Directory.GetCurrentDirectory()));
+			var content = engine.CompileRenderAsync("Template.cshtml", Enumerable.Range(0, 9)).Result;
+			File.WriteAllText("View.html", content);
+		}
+	}
+}
