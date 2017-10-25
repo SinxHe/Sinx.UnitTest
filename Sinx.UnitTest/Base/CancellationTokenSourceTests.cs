658a1a8dc5767d604be26c736b619e10056f7124
heshixiong <heshixiong@gridsum.com>
2017-10-25 13:20:04 +0800

    CancellationTokenSourceTests

---
+++ Sinx.UnitTest/Base/CancellationTokenSourceTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Xunit;
+
+namespace Sinx.UnitTest.Base
+{
+	public class CancellationTokenSourceTests
+	{
+		[Fact]
+		public void CancellationTokenSource_Thread4Callback()
+		{
+			int GetThreadId() => Thread.CurrentThread.ManagedThreadId;
+			var threadId = GetThreadId();
+			var token = new CancellationTokenSource();
+			var callThreadId = 0;
+			token.Token.Register(() =>
+			{
+				callThreadId = GetThreadId();
+			});
+			token.Cancel();
+			// 调用Cancel以后, 调用线程去执行注册的事件
+			Assert.NotEqual(threadId, 0);
+			Assert.Equal(callThreadId, threadId);
+		}
+	}
+}
