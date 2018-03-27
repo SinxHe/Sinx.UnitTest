18b2924fd2dfdcd398da5e692b737bc9d289597e
SinxHe <SinxHe@qq.com>
2018-03-27 23:37:50 +0800

    数组->原地算法

---
+++ Sinx.Algorithm/01ArrayTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Sinx.Algorithm
+{
+    public class _01ArrayTests
+    {
+        [InlineData(new[] { 1, 1, 2 })]
+        [InlineData(new[] { 1, 1, 2, 2 })]
+        [Theory]
+        public void Array_Reverse_Algorithm(int[] nums)
+        {
+            var expect = nums.Reverse().ToArray();
+            // 实验原地算法
+            var mid = (nums.Length - 1) / 2.0;
+            mid = mid % 1 > 0 ? (int)mid + 1 : mid;
+            for (int i = 0; i < mid; i++)
+            {
+                var temp = nums[i];
+                nums[i] = nums[nums.Length - i - 1];
+                nums[nums.Length - 1 - i] = temp;
+            }
+
+            Assert.Equal(nums, expect);
+        }
+
+        /// <summary>
+        /// 原地算法: https://baike.baidu.com/item/%E5%8E%9F%E5%9C%B0%E7%AE%97%E6%B3%95
+        /// 给定一个有序数组，你需要原地删除其中的重复内容，使每个元素只出现一次,并返回新的长度。
+        /// 不要另外定义一个数组，您必须通过用 O(1) 额外内存原地修改输入的数组来做到这一点。
+        /// 给定数组: nums = [1,1,2],
+        /// 你的函数应该返回新长度 2, 并且原数组nums的前两个元素必须是1和2
+        /// 不需要理会新的数组长度后面的元素
+        /// </summary>
+        [InlineData(new[] { 1, 1, 2 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 1, 2, 2 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 2, 3, 3, 4, 2, 1 }, new[] { 1, 2, 3, 4 })]
+        [InlineData(new[] { 1, 1, 2, 3 }, new[] { 1, 2, 3 })]
+        [Theory]
+        public void Array_Distinct_Algorithm(int[] nums, int[] expect)
+        {
+            // Act
+            var exchangeNum = 0;
+            // 箭头
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // 判断是否找完
+                if (i + exchangeNum >= nums.Length)
+                {
+                    break;
+                }
+                // 箭头左边 箭头L
+                for (int k = 0; k < i; k++)
+                {
+                    // 判断前面是不是有重复的
+                    // 有
+                    if (nums[k] == nums[i])
+                    {
+                        // 将这个元素放到从结尾往回滑动的元素位置
+                        for (int j = i; j < nums.Length - 1; j++)
+                        {
+                            var temp = nums[j];
+                            nums[j] = nums[j + 1];
+                            nums[j + 1] = temp;
+                        }
+                        exchangeNum++;
+                        // 对交换的元素再次进行验证
+                        i--;
+                    }
+                    // 没有
+                    else
+                    {
+                        // 箭头L向后移动
+                    }
+                }
+            }
+
+            var lengthOfNewArray = nums.Length - exchangeNum;
+            // Assert
+            Assert.Equal(nums.Take(lengthOfNewArray), expect);
+        }
+
+        [InlineData(new[] { 1, 1, 2 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 1, 2, 2 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 2, 3, 3, 4, 2, 1 }, new[] { 1, 2, 3, 4 })]
+        [InlineData(new[] { 1, 1, 2, 3 }, new[] { 1, 2, 3 })]
+        [Theory]
+        public void Array_Distinct_Algorithm_Best(int[] nums, int[] expect)
+        {
+            var len = nums.Length;
+            if (len != 0)
+            {
+                var count = 1;
+                for (var i = 1; i < len; i++)
+                {
+                    if (nums[i] == nums[i - 1]) continue;
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+            Assert.Equal(nums.Take(expect.Length), expect);
+        }
+    }
+}
--- Sinx.Concurrency.Tests/03 Rx/01 IObserable创建.cs
+++ Sinx.Concurrency.Tests/03 Rx/01 IObserable创建.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Reactive.Linq;
@@ -12,119 +10,142 @@ using Xunit;
 
 namespace Sinx.Concurrency.Tests._03_Rx
 {
-	public class _01_IObservable创建
-	{
-		[Fact]
-		public async Task IObservable_Create_Create()
-		{
-			// 没有输出
-			var observable = Observable.Create<int>(async o =>
-			{
-				foreach (var item in Enumerable.Range(0, 10))
-				{
-					var t = await Task.FromResult(item);
-					Console.WriteLine($"Create: {t}");
-					o.OnNext(t);
-				}
-			});
-			// 没有输出
-			IObservable<IList<int>> observableToList = observable.ToList();
-			// 进行等待, 然后有输出
-			await observableToList;
-			// 再调用一次, 又输出一次
-			var observableList = await observableToList;
-			foreach (var item in observableList)
-			{
-				Console.WriteLine($"Use: {item}");
-			}
-			// 边生成, 边使用
-			await observable.ForEachAsync(i => Console.WriteLine($"ForEachAsync: {i}"));
-		}
+    public class _01_IObservable创建
+    {
+        /// <summary>
+        /// Empty -> OnComplete
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public void IObservable_Empty()
+        {
+            var observable = Observable.Empty<int>();   // 冷启动
+            var dis = observable.Subscribe(Console.WriteLine, () => Console.WriteLine("Complete"));
+            dis.Dispose();  // 取消订阅
+        }
 
-		[Fact]
-		public async Task IObservable_Create_Range_UseCommonLinq()
-		{
-			IObservable<int> observable0 = Observable.Range(0, 10);
-			IObservable<string> observable1 = observable0.Where(i => i % 2 == 0)
-				.Select(i => i.ToString() + i.ToString());
-			// Obsolete
-			observable1.ForEach(i => Console.WriteLine($"ForEach: {i}"));
-			await observable1.ForEachAsync(i => Console.WriteLine($"ForEachAsync: {i}"));
-		}
+        /// <summary>
+        /// Return -> OnNext -> OnComplete
+        /// </summary>
+        [Fact]
+        public void IObservable_Return()
+        {
+            var observable = Observable.Return(1);
+            var dis = observable.Subscribe(Console.WriteLine, () => Console.WriteLine("Complete"));
+            dis.Dispose();
+        }
 
-		[Fact]
-		public async Task IObservable_Create_Interval()
-		{
-			IObservable<IList<long>> observable = Observable.Interval(TimeSpan.FromSeconds(1))
-				.Buffer(2);
-			IDisposable dis = observable.Subscribe(e => 
-					Console.WriteLine(DateTime.Now.Second + 
-					                $": Got {e[0]} and {e[1]}"));
-			Console.WriteLine("Not Finished");
-			//dis.Dispose(); // dispose以后就不会显示了
-			await observable;
-			Console.WriteLine("Finished");
-		}
+        [Fact]
+        public async Task IObservable_Create_Create()
+        {
+            // 没有输出
+            var observable = Observable.Create<int>(async o =>
+            {
+                foreach (var item in Enumerable.Range(0, 10))
+                {
+                    var t = await Task.FromResult(item);
+                    Console.WriteLine($"Create: {t}");
+                    o.OnNext(t);
+                }
+            });
+            // 没有输出
+            IObservable<IList<int>> observableToList = observable.ToList();
+            // 进行等待, 然后有输出
+            await observableToList;
+            // 再调用一次, 又输出一次
+            var observableList = await observableToList;
+            foreach (var item in observableList)
+            {
+                Console.WriteLine($"Use: {item}");
+            }
+            // 边生成, 边使用
+            await observable.ForEachAsync(i => Console.WriteLine($"ForEachAsync: {i}"));
+        }
 
-		/// <summary>
-		/// 从标准事件模式创建, 对于非标准的可以使用FromEvent
-		/// </summary>
-		/// <remarks>
-		/// 标准模式事件: 第一个参数是发送者, 第二个是参数
-		/// </remarks>
-		[Fact]
-		public void IObservable_Create_FromEventPattern()
-		{
-			// 新版EventHandler<T>
-			var progress = new Progress<int>();
-			var progressReports = Observable.FromEventPattern<int>(
-				handler => progress.ProgressChanged += handler,
-				handler => progress.ProgressChanged -= handler);
-			// 这里的EventArgs是强类型int
-			progressReports.Subscribe(data => Console.WriteLine($"OnNext: {data.EventArgs}"));
+        [Fact]
+        public async Task IObservable_Create_Range_UseCommonLinq()
+        {
+            IObservable<int> observable0 = Observable.Range(0, 10);
+            IObservable<string> observable1 = observable0.Where(i => i % 2 == 0)
+                .Select(i => i.ToString() + i.ToString());
+            // Obsolete
+            observable1.ForEach(i => Console.WriteLine($"ForEach: {i}"));
+            await observable1.ForEachAsync(i => Console.WriteLine($"ForEachAsync: {i}"));
+        }
 
-			// 旧版为每个事件定义一个委托类型
-			var timer = new System.Timers.Timer(100) { Enabled = true };
-			var ticks1 = Observable.FromEventPattern<ElapsedEventHandler, ElapsedEventArgs>(
-				// 转换器, EventHandler<ElapsedEventArgs> => ElapsedEventHandler
-				handler => (s, a) => handler(s, a),
-				handler => timer.Elapsed += handler,
-				handler => timer.Elapsed -= handler);
-			// EventArgs依旧是强类型, FromEventPattern的类型参数是对应的时间处理程序和EventArgs的派生类
-			ticks1.Subscribe(data => Console.WriteLine("ticks1 OnNext: " + data.EventArgs.SignalTime));
-			// ------ 使用反射 -------
-			var ticks2 = Observable.FromEventPattern(timer, nameof(timer.Elapsed));
-			ticks2.Subscribe(data => Console.WriteLine("ticks2 OnNext: " + ((ElapsedEventArgs)data.EventArgs).SignalTime));
+        [Fact]
+        public async Task IObservable_Create_Interval()
+        {
+            IObservable<IList<long>> observable = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Buffer(2);
+            IDisposable dis = observable.Subscribe(e =>
+                    Console.WriteLine(DateTime.Now.Second +
+                                    $": Got {e[0]} and {e[1]}"));
+            Console.WriteLine("Not Finished");
+            //dis.Dispose(); // dispose以后就不会显示了
+            await observable;
+            Console.WriteLine("Finished");
+        }
 
-			Thread.Sleep(1000);
-		}
+        /// <summary>
+        /// 从标准事件模式创建, 对于非标准的可以使用FromEvent
+        /// </summary>
+        /// <remarks>
+        /// 标准模式事件: 第一个参数是发送者, 第二个是参数
+        /// </remarks>
+        [Fact]
+        public void IObservable_Create_FromEventPattern()
+        {
+            // 新版EventHandler<T>
+            var progress = new Progress<int>();
+            var progressReports = Observable.FromEventPattern<int>(
+                handler => progress.ProgressChanged += handler,
+                handler => progress.ProgressChanged -= handler);
+            // 这里的EventArgs是强类型int
+            progressReports.Subscribe(data => Console.WriteLine($"OnNext: {data.EventArgs}"));
 
-		/// <summary>
-		/// 针对带有异常的AsyncCompletedEventArgs.Error事件, Rx会把这个作为一个数据事件
-		/// </summary>
-		[Fact]
-		public void IObservable_FromEventPattern_AsyncCompletedEventArgs_ErrorHandl()
-		{
-			var client = new WebClient();
-			var downloadedString = Observable.FromEventPattern(client, nameof(client.DownloadStringCompleted));
-			downloadedString.Subscribe(data =>
-			{
-				var eventArgs = (DownloadStringCompletedEventArgs)data.EventArgs;
-				if (eventArgs.Error != null)
-				{
-					Console.WriteLine("OnNext:(Error) " + eventArgs.Error);
-				}
-				else
-				{
-					Console.WriteLine("OnNext: " + eventArgs.Result);
-				}
-			},
-				ex => Console.WriteLine("OnError: " + ex.ToString()),
-				() => Console.WriteLine("OnCompleted"));
-			client.DownloadStringAsync(new Uri("http://baidu.com"));
-			Thread.Sleep(3000);
-			client.DownloadStringAsync(new Uri("http://invalid.example.com"));
-			Thread.Sleep(3000);
-		}
-	}
+            // 旧版为每个事件定义一个委托类型
+            var timer = new System.Timers.Timer(100) { Enabled = true };
+            var ticks1 = Observable.FromEventPattern<ElapsedEventHandler, ElapsedEventArgs>(
+                // 转换器, EventHandler<ElapsedEventArgs> => ElapsedEventHandler
+                handler => (s, a) => handler(s, a),
+                handler => timer.Elapsed += handler,
+                handler => timer.Elapsed -= handler);
+            // EventArgs依旧是强类型, FromEventPattern的类型参数是对应的时间处理程序和EventArgs的派生类
+            ticks1.Subscribe(data => Console.WriteLine("ticks1 OnNext: " + data.EventArgs.SignalTime));
+            // ------ 使用反射 -------
+            var ticks2 = Observable.FromEventPattern(timer, nameof(timer.Elapsed));
+            ticks2.Subscribe(data => Console.WriteLine("ticks2 OnNext: " + ((ElapsedEventArgs)data.EventArgs).SignalTime));
+
+            Thread.Sleep(1000);
+        }
+
+        /// <summary>
+        /// 针对带有异常的AsyncCompletedEventArgs.Error事件, Rx会把这个作为一个数据事件
+        /// </summary>
+        [Fact]
+        public void IObservable_FromEventPattern_AsyncCompletedEventArgs_ErrorHandl()
+        {
+            var client = new WebClient();
+            var downloadedString = Observable.FromEventPattern(client, nameof(client.DownloadStringCompleted));
+            downloadedString.Subscribe(data =>
+            {
+                var eventArgs = (DownloadStringCompletedEventArgs)data.EventArgs;
+                if (eventArgs.Error != null)
+                {
+                    Console.WriteLine("OnNext:(Error) " + eventArgs.Error);
+                }
+                else
+                {
+                    Console.WriteLine("OnNext: " + eventArgs.Result);
+                }
+            },
+                ex => Console.WriteLine("OnError: " + ex.ToString()),
+                () => Console.WriteLine("OnCompleted"));
+            client.DownloadStringAsync(new Uri("http://baidu.com"));
+            Thread.Sleep(3000);
+            client.DownloadStringAsync(new Uri("http://invalid.example.com"));
+            Thread.Sleep(3000);
+        }
+    }
 }
