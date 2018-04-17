3ae66d716f0d514ad579b6ef15fe0b75d6cbe081
heshixiong <heshixiong@gridsum.com>
2018-04-17 10:27:29 +0800

    bug fix

--- Sinx.AOP.Tests/Dora.Interception.Tests.cs
+++ Sinx.AOP.Tests/Dora.Interception.Tests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Dora.DynamicProxy;
 using Dora.Interception;
@@ -9,64 +7,63 @@ using Xunit;
 
 namespace Sinx.AOP.Tests
 {
-    public class Dora_Interception_Tests
-    {
-        private readonly IServiceProvider _sp;
-        public Dora_Interception_Tests()
-        {
-            var sp = new ServiceCollection()
-                .AddInterception()
-                .AddSingleton<IFoo, Foo>()
-                .BuildInterceptableServiceProvider();
-            _sp = sp;
-        }
+	public class Dora_Interception_Tests
+	{
+		private readonly IServiceProvider _sp;
+		public Dora_Interception_Tests()
+		{
+			var sp = new ServiceCollection()
+				.AddSingleton<IFoo, Foo>()
+				.BuildInterceptableServiceProvider();
+			_sp = sp;
+		}
 
-        [Fact]
-        public async Task Dora_Interception_Start()
-        {
-            var foo = _sp.GetRequiredService<IFoo>();
-            var r = await foo.GetAsync();
-            var dt = DateTimeOffset.Parse(Environment.GetEnvironmentVariable("Dora_Interception_Tests")??DateTimeOffset.Now.AddDays(-1).ToString());
-            Assert.Equal(1, r);
-            Assert.True(DateTimeOffset.Now - dt < TimeSpan.FromSeconds(2));
-        }
+		[Fact]
+		public async Task Dora_Interception_Start()
+		{
+			var foo = _sp.GetRequiredService<IFoo>();
+			await foo.GetAsync();
+			var dt = DateTimeOffset.Parse(Environment.GetEnvironmentVariable("Dora_Interception_Tests") ?? DateTimeOffset.Now.AddDays(-1).ToString());
+			Assert.True(DateTimeOffset.Now - dt < TimeSpan.FromSeconds(60));
+		}
 
-        private interface IFoo
-        {
-            [Decorate]
-            Task<int> GetAsync();
-        }
+		public interface IFoo
+		{
 
-        private class Foo : IFoo
-        {
-            public Task<int> GetAsync()
-            {
-                return Task.FromResult(1);
-            }
-        }
+			Task GetAsync();
+		}
 
-        private class DecorateAttribute : InterceptorAttribute
-        {
-            public override void Use(IInterceptorChainBuilder builder)
-            {
-                builder.Use<DecorateInterceptor>(Order);
-            }
-        }
+		public class Foo : IFoo
+		{
+			[Decorate]
+			public Task GetAsync()
+			{
+				return Task.CompletedTask;
+			}
+		}
+		[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
+		public class DecorateAttribute : InterceptorAttribute
+		{
+			public override void Use(IInterceptorChainBuilder builder)
+			{
+				builder.Use<DecorateInterceptor>(Order);
+			}
+		}
 
-        private class DecorateInterceptor
-        {
-            private readonly InterceptDelegate _next;
-            public DecorateInterceptor(InterceptDelegate next)
-            {
-                _next = next;
-            }
+		public class DecorateInterceptor
+		{
+			private InterceptDelegate _next;
+			public DecorateInterceptor(InterceptDelegate next)
+			{
+				_next = next;
+			}
 
-            public async Task InvokeAsync(InvocationContext context)
-            {
-                Environment.SetEnvironmentVariable("Dora_Interception_Tests", DateTimeOffset.Now.ToString());
-                await Task.Delay(TimeSpan.FromSeconds(1));
-                await _next(context);
-            }
-        }
-    }
+			public async Task InvokeAsync(InvocationContext context)
+			{
+				Environment.SetEnvironmentVariable("Dora_Interception_Tests", DateTimeOffset.Now.ToString());
+				await Task.Delay(TimeSpan.FromSeconds(1));
+				await _next(context);
+			}
+		}
+	}
 }
--- Sinx.Entry/Program.cs
+++ Sinx.Entry/Program.cs
@@ -1,17 +1,62 @@
-﻿using System.Diagnostics;
-using System.Threading;
+﻿using System;
 using System.Threading.Tasks;
+using Dora.DynamicProxy;
+using Dora.Interception;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Sinx.Entry
 {
-    public static class Program
-    {
-        public static void Main()
-        { 
-	        var context = SynchronizationContext.Current;
-	        var scheduler = TaskScheduler.Current;
-	        Debug.Assert(context != null);
-	        Debug.Assert(scheduler == null);
-        }
-    }
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			var demo = new ServiceCollection()
+					.AddSingleton<IDemo, Demo>()
+					.BuildInterceptableServiceProvider()
+					.GetRequiredService<IDemo>();
+			demo.InvokeAsync();
+			Console.WriteLine("Continue...");
+			Console.Read();
+		}
+		public class FoobarInterceptor
+		{
+			private InterceptDelegate _next;
+
+			public FoobarInterceptor(InterceptDelegate next)
+			{
+				_next = next;
+			}
+			public async Task InvokeAsync(InvocationContext context)
+			{
+				Console.WriteLine("Interception task starts.");
+				await Task.Delay(1000);
+				Console.WriteLine("Interception task completes.");
+				await _next(context);
+			}
+		}
+
+		[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
+		public class FoobarAttribute : InterceptorAttribute
+		{
+			public override void Use(IInterceptorChainBuilder builder)
+			{
+				builder.Use<FoobarInterceptor>(this.Order);
+			}
+		}
+
+		public interface IDemo
+		{
+			Task InvokeAsync();
+		}
+
+		public class Demo : IDemo
+		{
+			[Foobar]
+			public Task InvokeAsync()
+			{
+				Console.WriteLine("Target method is invoked.");
+				return Task.CompletedTask;
+			}
+		}
+	}
 }
