9f3a5ca2783c47920616f99fad983e06b9394fff
SinxHe <SinxHe@qq.com>
2018-05-22 21:56:21 +0800

    test: 石头与宝石

---
+++ Sinx.Algorithm/03旋转数组.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace Sinx.Algorithm
+{
+    public class _03旋转数组
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4 }, 1, new[] { 4, 1, 2, 3 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, 2, new[] { 3, 4, 1, 2 })]
+        public void Rotate(int[] nums, int k, int[] expect)
+        {
+            // Arrange & Act
+            for (int j = 0; j < k; j++)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    var temp = nums[0];
+                    nums[0] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+
+            // Assert
+            Assert.Equal(expect, nums);
+        }
+
+        //[Theory]
+        //[InlineData(new[] { 1, 2, 3, 4 }, 1, new[] { 4, 1, 2, 3 })]
+        //[InlineData(new[] { 1, 2, 3, 4 }, 2, new[] { 3, 4, 1, 2 })]
+        //public void Rotate2(int[] nums, int k, int[] expect)
+        //{
+        //    // Arrange & Act
+        //    var beginIndex = nums.Length - k - 1;
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        var temp = nums[i];
+        //        nums[i] = nums[beginIndex];
+        //        nums[beginIndex] = temp;
+        //        if (beginIndex == nums.Length)
+        //        {
+        //            beginIndex = 0;
+        //        }
+        //    }
+
+        //    // Assert
+        //    Assert.Equal(expect, nums);
+        //}
+    }
+}
---
+++ Sinx.Algorithm/771宝石与石头.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Sinx.Algorithm
+{
+    public class _771宝石与石头
+    {
+        [Theory]
+        [InlineData("aA", "aAAbbbb", 3)]
+        [InlineData("z", "ZZ", 0)]
+        public void NumJewelsInStones(string J, string S, int expect)
+        {
+            // Arrange & Act
+            int count = 0;
+            foreach (var sElement in S)
+            {
+                foreach (var jElement in J)
+                {
+                    if (jElement == sElement)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            // Assert
+            Assert.Equal(expect, count);
+        }
+    }
+}
