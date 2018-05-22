cea0025265205274c9504f18a5cf6c0935b58a12
SinxHe <SinxHe@qq.com>
2018-05-22 22:22:57 +0800

    test: leetcode 657判断路线成圈

---
+++ Sinx.Algorithm/657判断路线成圈.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Sinx.Algorithm
+{
+    public class _657判断路线成圈
+    {
+        [Theory]
+        [InlineData("U", false)]
+        [InlineData("ULDR", true)]
+        public void JudgeCircle(string moves, bool expect)
+        {
+            // Arrange
+            var horizontal = 0;
+            var vertical = 0;
+            foreach (var @char in moves)
+            {
+                switch (@char)
+                {
+                    case 'U':
+                        vertical += 1;
+                        break;
+                    case 'D':
+                        vertical += -1;
+                        break;
+                    case 'L':
+                        horizontal += -1;
+                        break;
+                    case 'R':
+                        horizontal += 1;
+                        break;
+                }
+            }
+
+            Assert.Equal(expect, horizontal + vertical == 0);
+        }
+    }
+}
