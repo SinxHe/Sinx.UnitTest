9b0de3d7596a81de316532e9d68feb85f1c59b1d
heshixiong <heshixiong@xiaobing.ai>
2022-05-22 21:28:10 +0800

    manim

---
+++ Sinx.PInvoke/Sinx.InvokeCpp.Cpp/Interface.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#if _MSC_VER    // TRUE for Microsoft compiler.
+#define DllExport  __declspec(dllexport)
+#else
+#define DllExport
+#endif
+
+
---
+++ Sinx.PInvoke/Sinx.PInvoke.CPP.Console/Sinx.PInvoke.CPP.Console.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "SinxClass.cpp"
+using namespace std;
+
+int main()
+{
+    auto sinx = new SinxClass();
+    sinx = nullptr;
+	try
+	{
+		cout << sinx->IntValue;
+	}
+	catch (const std::exception&)
+	{
+		cout << "exception";
+	}
+}
