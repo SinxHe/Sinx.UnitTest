0aeb327c1c72df0c1f6eed64a796150d3eb1a7de
何士雄 <heshixiong@xiaobing.ai>
2022-08-30 17:45:59 +0800

    intersection

---
+++ Sinx.LinearAlgebra/Shared/VectorExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Sinx.LinearAlgebra.Shared;
+
+public static class VectorExtensions
+{
+	public static Vector3 ToVector3(this Vector2 v)
+	{
+		return new(v.X, v.Y, 0);
+	}
+}
